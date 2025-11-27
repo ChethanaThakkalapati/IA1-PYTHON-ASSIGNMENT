5554370f7ddbe15c1fb6d797bf249031ada8fd93
Chethana Thakkalapti <chethanathakkelapati@gmail.com>
2025-11-27 07:51:33 +0530

    Fix formatting and clean up comments in qsn2.py

--- python assigment qsn2.py
+++ python assigment qsn2.py
@@ -1,4 +1,4 @@
-'''2.	An App for Mobile hosting provider charges- $0.51 per hour how much does it cost to operator per day, per week, per month? how many days can i operate one server with$918?'''
+'''An App for Mobile hosting provider charges- $0.51 per hour how much does it cost to operator per day, per week, per month? how many days can i operate one server with$918?'''
 cost_hour = 0.51
 
 cost_day = cost_hour * 24
@@ -14,4 +14,5 @@ print("Cost to operate per day: $", cost_day)
 print("Cost to operate per week: $", cost_week)
 print("Cost to operate per month: $", cost_month)
 print("Days you can operate with $918:", days_possible)
+
 
